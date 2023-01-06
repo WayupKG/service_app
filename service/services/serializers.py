7d7e1b7d3690a61eed704a56b9310a785a5d0db1
wayupkg <adikgk232323@gmail.com>
2023-01-06 15:39:34 +0600

    1 way to display prices using prefetch_related

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
 
-from services.models import Subscription
+from services.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = "__all__"
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email')
+        fields = ('id', 'client_name', 'email', 'price', 'plan')
+
+    def get_price(self, instance):
+        full_price = instance.service.full_price
+        return full_price - full_price * (instance.plan.discount_percent / 100)
