dbcfccbaee0222a0ad7132be0ce7a7e7780e95a3
wayupkg <adikgk232323@gmail.com>
2023-01-08 14:40:35 +0600

    Added Singleton for tasks and transaction for DB

--- service/service/celery.py
+++ service/service/celery.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 from celery import Celery
 
@@ -8,9 +7,3 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
 app = Celery('service')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-
-@app.task()
-def debug():
-    time.sleep(30)
-    print('Hello from debug task')
--- service/services/tasks.py
+++ service/services/tasks.py
@@ -1,14 +1,23 @@
 from celery import shared_task
+from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
 from service.celery import app
 
+"""
+select_for_update() - блокирует эту таблицу что бы пока он не закончил действие другие не могли изменить данные
+"""
 
-@app.task()
+
+@shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
-        annotate_price=F('service__full_price') - F('service__full_price') * (F('plan__discount_percent') / 100.00)
-    ).first()
-    subscription.price = subscription.annotate_price
-    subscription.save()
+
+    with transaction.atomic():
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotate_price=F('service__full_price') - F('service__full_price') * (F('plan__discount_percent') / 100.00)
+        ).first()
+        subscription.price = subscription.annotate_price
+        subscription.save()
+
